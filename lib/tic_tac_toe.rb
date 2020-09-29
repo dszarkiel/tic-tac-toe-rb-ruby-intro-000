cba24d7d58d64d8dbcaecb183c835468df6b7f31
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-09-29 19:46:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,7 +45,11 @@ def turn_count(board)
 end
 
 def current_player(board)
-  turn_count(board).even? ? 'X' : 'O'
+  if turn_count(board).even?
+    return "X"
+  else
+    return "O"
+  end
 end
 
 def turn(board)
